86d53caddad1d6d5f0f901b981e2bf5d26fe8688
Manuela Sanchez <m.sanchez947@Hotmail.com>
2021-06-07 12:56:45 -0400

    created left column in availibity

--- src/Astoria.js
+++ src/Astoria.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavBar from './NavBar.js'
+import { LoremIpsum } from 'react-lorem-ipsum';
 import './Apartments.css'
 
 class Astoria extends React.Component{
@@ -16,6 +17,11 @@ class Astoria extends React.Component{
         </div>
         <div className="header-image">
           <img src="https://www.cityguideny.com/columnpic/astoria.jpg" alt="bline img" height="90%" width="100%"/>
+        </div>
+        <div className="page-content">
+          <div className="title-text">
+            <LoremIpsum p={5} className="paragraph"/>
+            </div>
         </div>
       </div>
     )
--- src/Availability.js
+++ src/Availability.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavBar from './NavBar.js'
+import './Availability.css'
 
 class Availability extends React.Component{
   componentDidMount(){
@@ -13,6 +14,27 @@ class Availability extends React.Component{
         <div>
           <NavBar/>
         </div>
+        <section className="info">
+          <div className="container-availability">
+            <div className="inner">
+              <div className="left-column">
+                <div className="title">
+                  AVAILABILITY
+                </div>
+                <div className="description">
+                OCCUPANCY
+                <br/>
+                Model Residences Open By Appoitment
+                </div>
+                <div className="fact-sheet">
+                <a href="" > DOWNLOAD FACTSHEET</a>
+                </div>
+              </div>
+              <div className="right-column">
+              </div>
+            </div>
+          </div>
+        </section>
 
       </div>
     )
